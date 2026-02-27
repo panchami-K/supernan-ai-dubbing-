6e1fced786f5cd13b2b26a7bf5d716afaa1793ba
Panchami K <panchamik12345@gmail.com>
2026-02-27 06:50:55 +0000

    Step 1: Initial project setup

--- src/__init__.py
+++ src/__init__.py
@@ -6,17 +6,8 @@ Production-grade multilingual video dubbing
 __version__ = "1.0.0"
 __author__ = "Panchami K"
 
-from .config import Config, get_config
-from .video_utils import VideoProcessor, AudioProcessor, get_video_info, extract_segment
-from .transcribe import Transcriber, quick_transcribe
+# These will be imported once created
+# from .config import Config
+# from .pipeline import DubbingPipeline
 
-__all__ = [
-    "Config", 
-    "get_config",
-    "VideoProcessor",
-    "AudioProcessor", 
-    "get_video_info",
-    "extract_segment",
-    "Transcriber",
-    "quick_transcribe"
-]
+__all__ = []
